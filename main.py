ff325b9b6e8fb3f87a32e7a6a1da6f9f1f41e42f
donkledorf <stickeldr@gmail.com>
2024-10-14 13:53:03 -0700

    Chapter 2 Section 1

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ def main():
     print("Starting asteroids!")
 if __name__ == "__main__":
     main()
+    from constants import *
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
