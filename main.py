e1222dd7e3bdcf01f7eb13b95645207087d4e5f8
donkledorf <stickeldr@gmail.com>
2024-10-15 07:19:40 -0700

    debug

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0
-
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
