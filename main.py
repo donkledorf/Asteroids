2fb0dee6f0e97ffa26cdf75c5881df7c13c5bce6
donkledorf <stickeldr@gmail.com>
2024-10-14 20:20:59 -0700

    Chapter 2 Section 2 and major debugging

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pygame
+from constants import *
+BLACK = (0, 0, 0)
 def main():
+    pygame.init()
     print("Starting asteroids!")
-if __name__ == "__main__":
-    main()
-    from constants import *
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill(BLACK)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
+if __name__ == "__main__":
+        main()
