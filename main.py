7275471991abb60755db9b529e973580c2a9d490
donkledorf <stickeldr@gmail.com>
2024-10-14 20:43:19 -0700

    Chapter 2 Section 3

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         screen.fill(BLACK)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
         main()
