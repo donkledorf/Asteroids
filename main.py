47612dccdaf65940df9f07d6399d726d4d8cc058
donkledorf <stickeldr@gmail.com>
2024-10-19 12:46:45 -0700

    Chapter 3 Section 5

--- main.py
+++ main.py
@@ -38,6 +38,11 @@ def main():
             if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
+        for asteroid in asteroids:
+            for shot in shots:
+                 if asteroid.collides_with(shot):
+                    asteroid.kill()
+                    shot.kill()
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
