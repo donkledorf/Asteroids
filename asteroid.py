8cf24db8b5e51c28d86609370089a98bce573c5d
donkledorf <stickeldr@gmail.com>
2024-10-19 13:42:02 -0700

    Chapter 3 Section 6 and debug

--- asteroid.py
+++ asteroid.py
@@ -1,16 +1,29 @@
 import pygame
+import random
 from constants import *
 from circleshape import CircleShape
 class Asteroid(CircleShape):
-    def __init__(self, x, y, radius):
+    def __init__(self, x, y, radius, velocity):
         super().__init__(x, y, radius)
-        self.velocity = pygame.Vector2(0, 0)
+        self.velocity = pygame.Vector2(velocity)
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", (int(self.position.x), int(self.position.y)), int(self.radius), 2)
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20, 50)
+        vector_1 = self.velocity.rotate(random_angle)
+        vector_2 = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius, vector_1 * 1.2)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius, vector_2 * 1.2)
+        return asteroid1, asteroid2
 
 
     
